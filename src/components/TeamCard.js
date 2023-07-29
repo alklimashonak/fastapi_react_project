ef7afba41c8f2128a18e47779d5d7a1d262d6b4c
alkli <alklimashonak@gmail.com>
2023-07-29 12:23:41 +0200

    refactor

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,20 +1,43 @@
 import {Button, Container, ListGroup, ListGroupItem} from "react-bootstrap";
-import {useDrivers} from "../contexts/DriversProvider";
+ import {useApi} from "../contexts/ApiProvider";
 
-export default function TeamCard({teamDrivers, setTeamDrivers, picks, setPicks}) {
-    const {driversAll, setDriversAll} = useDrivers()
+export default function TeamCard({teamDrivers, setTeamDrivers, disabled, setDisabled, team}) {
+    const api = useApi()
 
     const handleButtonRemove = ({driver}) => {
-        if (picks.includes(driver.id)) {
-            const arr = picks.filter(item => item !== driver.id)
-            const newTeamDrivers = teamDrivers.filter(item => item.id !== driver.id)
-            const newDriversAll = driversAll.concat([driver])
-            setPicks(arr)
-            setDriversAll(newDriversAll)
-            setTeamDrivers(newTeamDrivers)
-        } else {
-            const arr = picks.concat([driver.id])
-            setPicks(arr)
+        if (teamDrivers.includes(driver)) {
+            setTeamDrivers(teamDrivers.filter(item => item.id !== driver.id))
+            setDisabled(disabled.filter(driverId => driverId !== driver.id))
+        }
+    }
+
+    const handleSaveButton = async () => {
+        const transfers = [];
+        const driversBefore = [];
+        const driversAfter = [];
+
+        team.drivers.map(driver => driversBefore.push(driver.id));
+        teamDrivers.map(driver => driversAfter.push(driver.id));
+
+        let driversIn = driversAfter.filter(driver => !driversBefore.includes(driver));
+        let driversOut = driversBefore.filter(driver => !driversAfter.includes(driver));
+
+        if (driversIn.length === driversOut.length) {
+            for (let i = 0; i < driversIn.length; i++) {
+                let transfer = {driver_in: driversIn[i], driver_out: driversOut[i]};
+                transfers.push(transfer)
+            }
+        }
+
+        const data = {
+            name: team.name,
+            transfers: transfers
+        };
+
+        if (disabled.length === 5 && transfers.length > 0) {
+             const response = await api.put(`/teams/${team.id}`, data);
+             response.ok ? console.log(response.body) : console.log(response)
+            window.location.reload();
         }
     }
 
@@ -29,13 +52,16 @@ export default function TeamCard({teamDrivers, setTeamDrivers, picks, setPicks})
                                         onClick={
                                             () => handleButtonRemove({driver: driver})
                                         }>
-                                        {picks.includes(driver.id) ? driver.last_name : null}
+                                        {driver.last_name}
                                     </Button>
                                 </ListGroupItem>
                             )
                         })
                     }
                 </ListGroup>
+                <Button type='submit' onClick={() => handleSaveButton()}>
+                    Save
+                </Button>
             </Container>
     )
 }
